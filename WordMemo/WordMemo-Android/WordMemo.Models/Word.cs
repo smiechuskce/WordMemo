c04048889d93b25a3dcae0df7c272305fc86508d
Kamil Śmieszek NTB <kamilsmieszek@gmail.com>
2017-10-06 08:29:51 +0200

    Added DB Access projects and changed models. Included SQLite.Net-PCL.

---
+++ WordMemo/WordMemo-Android/WordMemo.Models/Word.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite.Net.Attributes;
+using Xamarin.Forms;
+
+namespace WordMemo.Models
+{
+    public class Word
+    {
+        [PrimaryKey, AutoIncrement] // Really need the SQLite.NET-PCL reference?...
+        public int ID { get; set; }
+
+        public string BaseText { get; set; }
+
+        public string TranslationText { get; set; }
+
+        public Word()
+        {
+            
+        }
+    }
+}
---
+++ WordMemo/WordMemo-Android/WordMemo.ViewModels/WordViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordMemo.ViewModels
+{
+    public class WordViewModel
+    {
+        public int mWordID;
+
+        public string mWordBaseText;
+
+        public string mWordTranslationText;
+
+        public int WordID
+        {
+            get { return mWordID; }
+        }
+
+        public string WordBaseText
+        {
+            get { return mWordBaseText; }
+        }
+
+        public string WordTranslationText
+        {
+            get { return mWordTranslationText; }
+        }
+    }
+
+    public class Words
+    {
+        static WordViewModel[] _mBuiltInWordsViewModel =
+        {
+            new WordViewModel { mWordID = 1,
+                       mWordBaseText = "issue",
+                       mWordTranslationText = "problem" },
+
+            new WordViewModel { mWordID = 2,
+                       mWordBaseText = "tempered",
+                       mWordTranslationText = "hartowany" },
+
+            new WordViewModel { mWordID = 3,
+                       mWordBaseText = "tighten",
+                       mWordTranslationText = "dokręcać, napinać" }
+        };
+
+        private WordViewModel[] _mWordsViewModel;
+
+        public Words()
+        {
+            _mWordsViewModel = _mBuiltInWordsViewModel;
+        }
+
+        public int NumWords
+        {
+            get { return _mWordsViewModel.Length; }
+        }
+
+        public WordViewModel this[int i]
+        {
+            get { return _mWordsViewModel[i];  }
+        }
+    }
+}
