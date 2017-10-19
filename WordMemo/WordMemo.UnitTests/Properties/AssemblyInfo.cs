88babf531c8bc8633620c0940c95d495984d17d5
Kamil Śmieszek NTB <kamilsmieszek@gmail.com>
2017-10-19 13:27:03 +0200

    Changed project structure.

--- WordMemo/WordMemo.UnitTests/Properties/AssemblyInfo.cs
+++ WordMemo/WordMemo.UnitTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WordMemo.UnitTests")]
+[assembly: AssemblyTitle("WordMemo.LogicTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WordMemo.UnitTests")]
+[assembly: AssemblyProduct("WordMemo.LogicTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e541a5a4-fc48-4df4-81db-482e2a127b62")]
 
 // Version information for an assembly consists of the following four values:
 //
--- WordMemo/WordMemo.UnitTests/WordTests.cs
+++ WordMemo/WordMemo.UnitTests/WordTests.cs
@@ -1,19 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using WordMemo.ViewModels;
-
 
 namespace WordMemo.UnitTests
 {
     [TestFixture]
     public class WordTests
     {
-        [Test]
-        public void InitialWordListIsNotEmpty()
+        public WordManager WordManager = new WordManager();
+
+        [SetUp]
+        public void Init()
         {
-            Words allWords = new Words();
-            
-            Assert.GreaterOrEqual(allWords.NumWords, 0);      
+            WordManager.Init();
+        }
+
+        [Test]
+        public void initial_word_list_is_not_empty()
+        {
+            // Arrange
+            // may be empty?
+
+            // Act
+            var wordsCount = WordManager.NumWords;
+
+            // Assert
+            Assert.GreaterOrEqual(wordsCount, 0);
+        }
+
+        [Test]
+        public void initial_word_list_contains_more_than_3_words()
+        {
+            WordManager.Add(new Word(4, "test", "testować"));
+
+            var wordsCount = WordManager.NumWords;
+
+            Assert.GreaterOrEqual(wordsCount, 4);
         }
     }
 }
